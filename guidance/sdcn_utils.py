5376d6cf8cc8b3dbfae61f8880663ec1371d1776
xucr <xucr@shanghaitech.edu.cn>
2023-11-26 21:11:29 -0800

    Add control net support

--- guidance/sdcn_utils.py
+++ guidance/sdcn_utils.py
@@ -9,10 +9,13 @@ from diffusers import (
 )
 from diffusers.utils.import_utils import is_xformers_available
 from diffusers.utils.torch_utils import randn_tensor
-from mvdream.ldm.models.diffusion.ddim import DDIMSampler
-from mvdream.camera_utils import get_camera, convert_opengl_to_blender, normalize_camera
 from diffusers.image_processor import VaeImageProcessor
+from openpose_utils import *
+from cam_utils import orbit_camera
+from gs_renderer import MiniCam
+from openpose_utils import *
 
+import numpy as np
 
 # suppress partial model loading warning
 logging.set_verbosity_error()
@@ -96,132 +99,182 @@ class StableDiffusionControlNet(nn.Module):
         embeddings = self.model.get_learned_conditioning(prompt).to(self.device)
         return embeddings
 
-    @torch.no_grad()
-    def refine(
-        self,
-        pred_rgb,
-        camera,
-        guidance_scale=100,
-        steps=50,
-        strength=0.8,
-    ):
-        batch_size = pred_rgb.shape[0]
-        pred_rgb_256 = F.interpolate(
-            pred_rgb, (256, 256), mode="bilinear", align_corners=False
-        )
-        latents = self.encode_imgs(pred_rgb_256.to(self.dtype))
-        # latents = torch.randn((1, 4, 64, 64), device=self.device, dtype=self.dtype)
+    # @torch.no_grad()
+    # def refine(
+    #     self,
+    #     pred_rgb,
+    #     camera,
+    #     guidance_scale=100,
+    #     steps=50,
+    #     strength=0.8,
+    # ):
+    #     batch_size = pred_rgb.shape[0]
+    #     pred_rgb_256 = F.interpolate(
+    #         pred_rgb, (256, 256), mode="bilinear", align_corners=False
+    #     )
+    #     latents = self.encode_imgs(pred_rgb_256.to(self.dtype))
+    #     # latents = torch.randn((1, 4, 64, 64), device=self.device, dtype=self.dtype)
 
-        self.scheduler.set_timesteps(steps)
-        init_step = int(steps * strength)
-        latents = self.scheduler.add_noise(
-            latents, torch.randn_like(latents), self.scheduler.timesteps[init_step]
-        )
+    #     self.scheduler.set_timesteps(steps)
+    #     init_step = int(steps * strength)
+    #     latents = self.scheduler.add_noise(
+    #         latents, torch.randn_like(latents), self.scheduler.timesteps[init_step]
+    #     )
 
-        camera = camera[:, [0, 2, 1, 3]]  # to blender convention (flip y & z axis)
-        camera[:, 1] *= -1
-        camera = normalize_camera(camera).view(batch_size, 16)
-        camera = camera.repeat(2, 1)
-        context = {"context": self.embeddings, "camera": camera, "num_frames": 4}
+    #     camera = camera[:, [0, 2, 1, 3]]  # to blender convention (flip y & z axis)
+    #     camera[:, 1] *= -1
+    #     camera = normalize_camera(camera).view(batch_size, 16)
+    #     camera = camera.repeat(2, 1)
+    #     context = {"context": self.embeddings, "camera": camera, "num_frames": 4}
 
-        for i, t in enumerate(self.scheduler.timesteps[init_step:]):
-            latent_model_input = torch.cat([latents] * 2)
+    #     for i, t in enumerate(self.scheduler.timesteps[init_step:]):
+    #         latent_model_input = torch.cat([latents] * 2)
 
-            tt = torch.cat([t.unsqueeze(0).repeat(batch_size)] * 2).to(self.device)
+    #         tt = torch.cat([t.unsqueeze(0).repeat(batch_size)] * 2).to(self.device)
 
-            noise_pred = self.model.apply_model(latent_model_input, tt, context)
+    #         noise_pred = self.model.apply_model(latent_model_input, tt, context)
 
-            noise_pred_uncond, noise_pred_cond = noise_pred.chunk(2)
-            noise_pred = noise_pred_uncond + guidance_scale * (
-                noise_pred_cond - noise_pred_uncond
-            )
+    #         noise_pred_uncond, noise_pred_cond = noise_pred.chunk(2)
+    #         noise_pred = noise_pred_uncond + guidance_scale * (
+    #             noise_pred_cond - noise_pred_uncond
+    #         )
 
-            latents = self.scheduler.step(noise_pred, t, latents).prev_sample
+    #         latents = self.scheduler.step(noise_pred, t, latents).prev_sample
 
-        imgs = self.decode_latents(latents)  # [1, 3, 512, 512]
-        return imgs
+    #     imgs = self.decode_latents(latents)  # [1, 3, 512, 512]
+    #     return imgs
 
     def train_step(
         self,
         pred_rgb,  # TODO:This can be [B,C,H,W] or [C,H*N_1,W*N_2], modify the code accordingly
         camera,  # TODO: [B, 4, 4], modify the code accordingly
+        cur_cam,
         step_ratio=None,
         guidance_scale=7.5,
         as_latent=False,
+        hors=None,
     ):
-        # batch_size = pred_rgb.shape[0]
+        batch_size = pred_rgb.shape[0]
         # TODO: get the width and height of the generate image
-        # width=???
-        # height=???
+        width = 512
+        height = 512
 
         pred_rgb = pred_rgb.to(self.dtype)
 
         if as_latent:
             latents = (
-                F.interpolate(pred_rgb, (32, 32), mode="bilinear", align_corners=False)
+                F.interpolate(pred_rgb, (64, 64), mode="bilinear", align_corners=False)
                 * 2
                 - 1
             )
         else:
-            # interp to 256x256 to be fed into vae.
-            # TODO:Check the width and height of the generate image
-            pred_rgb_256 = F.interpolate(
-                pred_rgb, (width, height), mode="bilinear", align_corners=False
+            # interp to 512x512 to be fed into vae.
+            pred_rgb_512 = F.interpolate(
+                pred_rgb, (512, 512), mode="bilinear", align_corners=False
             )
             # encode image into latents with vae, requires grad!
-            latents = self.encode_imgs(pred_rgb_256)
-            num_images_per_prompt = 1
-            batch_size = 1
+            latents = self.encode_imgs(pred_rgb_512)
 
-        if step_ratio is not None:
-            # dreamtime-like
-            # t = self.max_step - (self.max_step - self.min_step) * np.sqrt(step_ratio)
-            t = np.round((1 - step_ratio) * self.num_train_timesteps).clip(
-                self.min_step, self.max_step
-            )
-            t = torch.full((batch_size,), t, dtype=torch.long, device=self.device)
-        else:
-            t = torch.randint(
-                self.min_step,
-                self.max_step + 1,
-                (batch_size,),
-                dtype=torch.long,
-                device=self.device,
-            )
-        # 7.1 Create tensor stating which controlnets to keep
-        # controlnet_keep = []
-        # for i in range(len(timesteps)):
-        #     keeps = [
-        #         1.0 - float(i / len(timesteps) < s or (i + 1) / len(timesteps) > e)
-        #         for s, e in zip(control_guidance_start, control_guidance_end)
-        #     ]
-        #     controlnet_keep.append(
-        #         keeps[0] if isinstance(controlnet, ControlNetModel) else keeps
-        #     )
-
-        # camera = convert_opengl_to_blender(camera)
-        # flip_yz = torch.tensor([[1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 1]]).unsqueeze(0)
-        # camera = torch.matmul(flip_yz.to(camera), camera)
-
-        camera = camera[:, [0, 2, 1, 3]]  # to blender convention (flip y & z axis)
-        camera[:, 1] *= -1
-
-        camera = normalize_camera(camera).view(batch_size, 16)
-
-        camera = camera.repeat(2, 1)
-
-        # TODO:Base on the camera to generate the openpose images, blender convention!!!
-        # image= ???
-
-        # predict the noise residual with unet, NO grad!
         with torch.no_grad():
+            if step_ratio is not None:
+                # dreamtime-like
+                # t = self.max_step - (self.max_step - self.min_step) * np.sqrt(step_ratio)
+                t = np.round((1 - step_ratio) * self.num_train_timesteps).clip(
+                    self.min_step, self.max_step
+                )
+                t = torch.full((batch_size,), t, dtype=torch.long, device=self.device)
+            else:
+                t = torch.randint(
+                    self.min_step,
+                    self.max_step + 1,
+                    (batch_size,),
+                    dtype=torch.long,
+                    device=self.device,
+                )
+
+            # 7.1 Create tensor stating which controlnets to keep
+            # controlnet_keep = []
+            # for i in range(len(timesteps)):
+            #     keeps = [
+            #         1.0 - float(i / len(timesteps) < s or (i + 1) / len(timesteps) > e)
+            #         for s, e in zip(control_guidance_start, control_guidance_end)
+            #     ]
+            #     controlnet_keep.append(
+            #         keeps[0] if isinstance(controlnet, ControlNetModel) else keeps
+            #     )
+
+            # camera = convert_opengl_to_blender(camera)
+            # flip_yz = torch.tensor([[1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 1]]).unsqueeze(0)
+            # camera = torch.matmul(flip_yz.to(camera), camera)
+            T_pose_keypoints = np.array(
+                [
+                    [0, 158, 14],
+                    [0, 138, 0],
+                    [-17, 138, 0],
+                    [-17, 113, 0],
+                    [-17, 88, 0],
+                    [17, 138, 0],
+                    [17, 113, 0],
+                    [17, 88, 0],
+                    [-10, 92, 0],
+                    [-10, 52, 0],
+                    [-10, 16, 0],
+                    [10, 92, 0],
+                    [10, 52, 0],
+                    [10, 16, 0],
+                    [-3, 161, 11],
+                    [3, 161, 11],
+                    [-7, 158, 3],
+                    [7, 158, 3],
+                ]
+            )
+
+            normalized_keypoints = mid_and_scale(T_pose_keypoints)
+            w2c = np.linalg.inv(camera)
+            w2c[1:3, :3] *= -1
+            w2c[:3, 3] *= -1
+
+            K = cur_cam.K()
+            RT = w2c[:3, :]
+            # TODO:Base on the camera to generate the openpose images, blender convention!!!
+
+            if hors is None:
+                embeddings = torch.cat(
+                    [
+                        self.embeddings["pos"].expand(batch_size, -1, -1),
+                        self.embeddings["neg"].expand(batch_size, -1, -1),
+                    ]
+                )
+            else:
+
+                def _get_dir_ind(h):
+                    if abs(h) < 60:
+                        return "front"
+                    elif abs(h) < 120:
+                        return "side"
+                    else:
+                        return "back"
+
+                embeddings = torch.cat(
+                    [self.embeddings[_get_dir_ind(h)] for h in hors]
+                    + [self.embeddings["neg"].expand(batch_size, -1, -1)]
+                )
+
+            image = draw_openpose_human_pose(
+                normalized_keypoints,
+                (512, 512),
+                K,
+                RT,
+            )
+            # predict the noise residual with unet, NO grad!
+
             # add noise
             noise = torch.randn_like(latents)
             latents_noisy = self.scheduler.add_noise(latents, noise, t)
             # pred noise
             latent_model_input = torch.cat([latents_noisy] * 2)
             tt = torch.cat([t] * 2)
-
+            w = (1 - self.alphas[t]).view(batch_size, 1, 1, 1)
             # Run controlnet
 
             image = self.prepare_image(
@@ -238,10 +291,10 @@ class StableDiffusionControlNet(nn.Module):
             height, width = image.shape[-2:]
 
             control_model_input = latent_model_input
-            controlnet_prompt_embeds = self.embeddings
+            controlnet_prompt_embeds = embeddings
             down_block_res_samples, mid_block_res_sample = self.controlnet(
                 control_model_input,
-                t,
+                tt,
                 encoder_hidden_states=controlnet_prompt_embeds,
                 controlnet_cond=image,
                 # conditioning_scale=cond_scale,
@@ -256,21 +309,21 @@ class StableDiffusionControlNet(nn.Module):
             # ).sample
             noise_pred = self.unet(
                 latent_model_input,
-                t,
-                encoder_hidden_states=self.embeddings,
+                tt,
+                encoder_hidden_states=embeddings,
                 # timestep_cond=timestep_cond,
                 # cross_attention_kwargs=cross_attention_kwargs,
                 down_block_additional_residuals=down_block_res_samples,
                 mid_block_additional_residual=mid_block_res_sample,
-                return_dict=False,
             ).sample
+
             # perform guidance (high scale from paper!)
             noise_pred_uncond, noise_pred_pos = noise_pred.chunk(2)
             noise_pred = noise_pred_uncond + guidance_scale * (
                 noise_pred_pos - noise_pred_uncond
             )
 
-        grad = noise_pred - noise
+        grad = w * (noise_pred - noise)
         grad = torch.nan_to_num(grad)
 
         # seems important to avoid NaN...
@@ -285,6 +338,7 @@ class StableDiffusionControlNet(nn.Module):
 
         return loss
 
+    @torch.no_grad()
     def prepare_latents(
         self,
         batch_size,
