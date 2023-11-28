6fe29196a57bc75720ae12ea7d153a6f23995253
shizhanhe <142436587@qq.com>
2023-11-28 03:27:04 -0800

    image2keypoint3D

---
+++ img2keypoints.py
@@ -0,0 +1,149 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+import math
+import os
+import sys
+import os.path as osp
+import torch
+from torchvision.transforms import Normalize
+import numpy as np
+import cv2
+import argparse
+import json
+import pickle
+from datetime import datetime
+import pickle as pkl
+
+from frankmocap.demo.demo_options import DemoOptions
+from frankmocap.bodymocap.body_mocap_api import BodyMocap
+from frankmocap.bodymocap.body_bbox_detector import BodyPoseEstimator
+import frankmocap.mocap_utils.demo_utils as demo_utils
+import frankmocap.mocap_utils.general_utils as gnu
+from frankmocap.mocap_utils.timer import Timer
+
+import frankmocap.renderer.image_utils as imu
+from frankmocap.renderer.viewer2D import ImShow
+
+from scipy import sparse
+
+
+def run_body_mocap(body_bbox_detector, body_mocap, visualizer, image_path, out_dir=None):
+    # Setup input data to handle different types of inputs
+
+    timer = Timer()
+
+    img_original_bgr = image_path
+
+    body_pose_list, body_bbox_list = body_bbox_detector.detect_body_pose(
+        img_original_bgr)
+
+    if len(body_bbox_list) < 1:
+        print(f"No body deteced!")
+        return
+
+        # Sort the bbox using bbox size
+    # (to make the order as consistent as possible without tracking)
+    bbox_size = [(x[2] * x[3]) for x in body_bbox_list]
+    idx_big2small = np.argsort(bbox_size)[::-1]
+    body_bbox_list = [body_bbox_list[i] for i in idx_big2small]
+    body_bbox_list = [body_bbox_list[0], ]
+
+    # Body Pose Regression
+    pred_output_list = body_mocap.regress(img_original_bgr, body_bbox_list)
+    assert len(body_bbox_list) == len(pred_output_list)
+
+    # extract mesh for rendering (vertices in image space and faces) from pred_output_list
+    pred_mesh_list = demo_utils.extract_mesh_from_output(pred_output_list)
+    pred_mesh_list[0]['vertices'] = pred_mesh_list[0]['vertices'] - np.mean(pred_mesh_list[0]['vertices'], axis=0,
+                                                                            keepdims=True)
+
+    if out_dir:
+        gnu.save_mesh_to_obj(os.path.join(out_dir, 'mesh.obj'), pred_mesh_list[0]['vertices'],
+                             pred_mesh_list[0]['faces'])
+
+    timer.toc(bPrint=True, title="Time")
+
+    # return openpose body25 keypoints
+    return pred_mesh_list[0]['vertices']
+
+
+def image2keypoint(image: np.ndarray, out_dir=None):
+    """
+    Generate keypoints from image. First using frankmocap to get body mesh, then using body25 regressor get keypoints.
+    Args:
+        image: read from cv2.imread, BGR format
+        out_dir: directory to save mesh.obj. leave None if not save
+
+    Returns:
+        np.array, shape=(25, 3), body25 keypoints of centered mesh
+    """
+    args = DemoOptions().parse()
+
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    assert torch.cuda.is_available(), "Current version only supports GPU"
+
+    # Set bbox detector
+    body_bbox_detector = BodyPoseEstimator()
+
+    # Set mocap regressor
+    use_smplx = False
+    checkpoint_path = './frankmocap/extra_data/body_module/pretrained_weights/2020_05_31-00_50_43-best-51.749683916568756.pt'
+
+    body_mocap = BodyMocap(checkpoint_path, "./frankmocap/extra_data/smpl/", device, use_smplx)
+
+    # Set Visualizer
+
+    # from frankmocap.renderer.visualizer import Visualizer
+    # visualizer = Visualizer('opengl')
+
+    smpl = run_body_mocap(body_bbox_detector, body_mocap, None, image, out_dir)
+    # center mesh
+    body25_reg = pkl.load(
+        open(('frankmocap/extra_data/smpl/body25_regressor.pkl'), 'rb'), encoding="latin1").T
+    body = sparse.csr_matrix.dot(body25_reg, smpl)
+    return body
+
+
+if __name__ == '__main__':
+    img = cv2.imread('frankmocap/sample_data/IMG_0871.JPG')
+    out_dir = 'frankmocap/output'
+    res = image2keypoint(img, out_dir)
+    print(res)
+
+    from cam_utils import orbit_camera
+    from openpose_utils import *
+    import cv2
+    import numpy as np
+
+    normalized_keypoints = mid_and_scale(res)
+
+    fovy = 49.1
+    pose = orbit_camera(-0, 0, 3.5)
+    w2c = np.linalg.inv(pose)
+    w2c[1:3, :3] *= -1
+    w2c[:3, 3] *= -1
+    # print(pose)
+
+    print("fovy:", np.deg2rad(fovy))
+    K = np.zeros((3, 3))
+    K[0, 0] = 512 / math.tan(np.deg2rad(fovy) / 2)
+    K[1, 1] = 512 / math.tan(np.deg2rad(fovy) / 2)
+    K[0, 2] = 512 / 2
+    K[1, 2] = 512 / 2
+    K[2, 2] = 1
+    RT = w2c[:3, :]
+    # focal_length = 500
+    # fx = focal_length * image_shape[1] / image_shape[0]
+    # fy = focal_length
+    # cx, cy = image_shape[1] / 2, image_shape[0] / 2  # 光心
+    # K = np.array([[fx, 0, cx], [0, fy, cy], [0, 0, 1]])
+    print(K)
+    print(RT)
+    result = draw_openpose_human_pose_official(K, RT, normalized_keypoints, (512, 512), )
+    # up-down flip
+    result = cv2.flip(result, 0)
+    # cv2.imwrite("T_pose.jpg", cv2.cvtColor(result, cv2.COLOR_BGR2RGB))
+    # show result
+    import matplotlib.pyplot as plt
+
+    plt.imshow(result)
+    plt.show()
--- openpose_utils.py
+++ openpose_utils.py
@@ -58,7 +58,7 @@ def draw_openpose_human_pose(K, RT, keypoints=None, image_shape=(512, 512), alph
     )
 
     normalized_keypoints = mid_and_scale(T_pose_keypoints)
-    if keypoints == None:
+    if keypoints is None:
         keypoints = normalized_keypoints
 
     # Openpose colors, one for each keypoint(25)
@@ -126,6 +126,9 @@ def draw_openpose_human_pose(K, RT, keypoints=None, image_shape=(512, 512), alph
         [15, 17],
     ]
 
+
+
+
     # Draw keypoints
     for i, keypoint in enumerate(keypoints):
         overlay = image.copy()
@@ -178,6 +181,146 @@ def draw_openpose_human_pose(K, RT, keypoints=None, image_shape=(512, 512), alph
 
     return image
 
+def draw_openpose_human_pose_official(K, RT, keypoints=None, image_shape=(512, 512), alpha=0.5):
+    """
+    Draw Openpose human pose on image
+    Args:
+        keypoints (numpy array): Openpose keypoints (25, 3) (index, x, y, z)
+        image_shape (tuple): Image shape (height, width)
+        K (numpy array): Camera intrinsic matrix (3, 3)
+        RT (numpy array): Camera extrinsic matrix (3, 4)
+        alpha (float): Opacity of keypoints
+    """
+    # Create image
+    image = np.zeros((image_shape[0], image_shape[1], 3), dtype=np.uint8)
+    T_pose_keypoints = np.array(
+        [
+            [0, 158, 14],
+            [0, 138, 0],
+            [-17, 138, 0],
+            [-17, 113, 0],
+            [-17, 88, 0],
+            [17, 138, 0],
+            [17, 113, 0],
+            [17, 88, 0],
+            [-10, 92, 0],
+            [-10, 52, 0],
+            [-10, 16, 0],
+            [10, 92, 0],
+            [10, 52, 0],
+            [10, 16, 0],
+            [-3, 161, 11],
+            [3, 161, 11],
+            [-7, 158, 3],
+            [7, 158, 3],
+        ]
+    )
+
+    normalized_keypoints = mid_and_scale(T_pose_keypoints)
+    if keypoints is None:
+        keypoints = normalized_keypoints
+
+    # Openpose colors, one for each keypoint(25)
+    # openpose_colors = [
+    #     (255, 0, 0), (255, 85, 0), (255, 170, 0), (255, 255, 0), (170, 255, 0),
+    #     (85, 255, 0), (0, 255, 0), (0, 255, 85), (0, 255, 170), (0, 255, 255),
+    #     (0, 170, 255), (0, 85, 255), (0, 0, 255), (85, 0, 255), (170, 0, 255),
+    #     (255, 0, 255), (255, 0, 170), (255, 0, 85), (255, 0, 0), (255, 85, 0),
+    #     (255, 170, 0), (255, 255, 0), (170, 255, 0), (85, 255, 0), (0, 255, 0)
+    # ]
+
+    # openpose_conn = [
+    #     [0, 1], [1, 2], [1, 5], [2, 3], [3, 4], [5, 6], [6, 7],
+    #     [1, 8], [8, 9], [9, 10], [10, 11], [8, 12], [12, 13],
+    #     [13, 14], [0, 15], [0, 16], [15, 17], [16, 18], [11, 24],
+    #     [11, 22], [22, 23], [14, 21], [14, 19], [19, 20]
+    # ]
+
+    # color for each keypoint(18)
+    openpose_colors = [
+        (255, 0, 85),
+        (255, 0, 0),
+        (255, 85, 0),
+        (255, 170, 0),
+        (255, 255, 0),
+        (170, 255, 0),
+        (85, 255, 0),
+        (0, 255, 0),
+        (255, 0, 0),
+        (0, 255, 85),
+        (0, 255, 170),
+        (0, 255, 255),
+        (0, 170, 255),
+        (0, 85, 255),
+        (0, 0, 255),
+        (255, 0, 170),
+        (170, 0, 255),
+        (255, 0, 255),
+        (85, 0, 255),
+        (0, 0, 255),
+        (0, 0, 255),
+        (0, 0, 255),
+        (0, 255, 255),
+        (0, 255, 255),
+        (0, 255, 255),
+    ]
+
+    # openpose official connection TODO: check color order
+    openpose_conn = np.array(
+        [1, 8, 1, 2, 1, 5, 2, 3, 3, 4, 5, 6, 6, 7, 8, 9, 9, 10, 10, 11, 8, 12, 12, 13, 13, 14, 1, 0, 0, 15, 15, 17, 0,
+         16, 16, 18, 14, 19, 19, 20, 14, 21, 11, 22, 22, 23, 11, 24]).reshape(-1, 2)
+
+    # Draw keypoints
+    for i, keypoint in enumerate(keypoints):
+        overlay = image.copy()
+        # project 3D point to 2D
+        point_3d = np.array([keypoint[0], keypoint[1], keypoint[2], 1])
+        point_2d = K @ RT @ point_3d
+        point_2d = point_2d[:2] / point_2d[2]
+        # Draw circle
+        cv2.circle(
+            overlay, (int(point_2d[0]), int(point_2d[1])), 4, openpose_colors[i], -1
+        )
+        # Blend images
+        image = cv2.addWeighted(image, 1 - alpha, overlay, alpha, 0)
+
+    # Draw connections(not line, but oval)
+    for connection in openpose_conn:
+        num1, num2 = connection
+        if num1 >= len(keypoints) or num2 >= len(keypoints):
+            continue
+        overlay = image.copy()
+        # project 3D point to 2D
+        point_3d_1 = np.array(
+            [keypoints[num1][0], keypoints[num1][1], keypoints[num1][2], 1]
+        )
+        point_2d_1 = K @ RT @ point_3d_1
+        point_2d_1 = point_2d_1[:2] / point_2d_1[2]
+        point_3d_2 = np.array(
+            [keypoints[num2][0], keypoints[num2][1], keypoints[num2][2], 1]
+        )
+        point_2d_2 = K @ RT @ point_3d_2
+        point_2d_2 = point_2d_2[:2] / point_2d_2[2]
+        # Draw oval
+        midpoint = (point_2d_1 + point_2d_2) / 2
+        angle = np.degrees(
+            np.arctan2(point_2d_2[1] - point_2d_1[1], point_2d_2[0] - point_2d_1[0])
+        )
+        thickness = 3
+        cv2.ellipse(
+            overlay,
+            (int(midpoint[0]), int(midpoint[1])),
+            (int(np.linalg.norm(point_2d_2 - point_2d_1) / 2), thickness),
+            angle,
+            0,
+            360,
+            openpose_colors[num2],
+            -1,
+        )
+        # Blend images
+        image = cv2.addWeighted(image, 1 - alpha, overlay, alpha, 0)
+
+    return image
 
 def is_back(K, RT) -> bool:
     """
@@ -196,3 +339,4 @@ def is_back(K, RT) -> bool:
     forward = K @ forward
     # Check if camera is facing back
     return not (C[2] < 0 and forward[2] < 0)
+
