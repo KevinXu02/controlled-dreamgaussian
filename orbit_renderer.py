a3721c1c4541527f84a26eabcfe2db186d55c46c
xucr <xucr@shanghaitech.edu.cn>
2023-12-04 23:15:23 -0800

    Add function to render GIFs

--- orbit_renderer.py
+++ orbit_renderer.py
@@ -50,8 +50,42 @@ def render_orbit_imgs(load_path):
         img.save(f"renders/{i}.png")
 
 
+def render_gif(load_path):
+    renderer = Renderer(sh_degree=0)
+    renderer.initialize(load_path)
+    poses = []
+    imgs = []
+    for hor in range(-180, 179, 10):
+        pose = orbit_camera(-15, hor, 3)
+        poses.append(pose)
+
+    fovy = 50
+    for i, pose in enumerate(poses):
+        # print(pose)
+        cur_cam = MiniCam(
+            pose,
+            512,
+            512,
+            np.deg2rad(fovy),
+            np.deg2rad(fovy),
+            0.01,
+            100,
+        )
+        bg_color = torch.tensor([1, 1, 1], dtype=torch.float32, device="cuda")
+        out = renderer.render(cur_cam, bg_color=bg_color)
+        img = out["image"].unsqueeze(0)[0]
+        img = img.detach().permute(1, 2, 0).cpu().numpy()
+        img = (img * 255).astype(np.uint8)
+        imgs.append(img)
+    # use imageio to create gif
+    import imageio
+
+    imageio.mimsave("renders/orbit.gif", imgs, fps=30)
+
+
 # main
 if __name__ == "__main__":
     load_path = "logs\saved_models\darthvader_3_model.ply"
     render_orbit_imgs(load_path)
     grid_image = image_utils.resize_and_fit_images("renders", "renders/gird.png")
+    render_gif(load_path)
